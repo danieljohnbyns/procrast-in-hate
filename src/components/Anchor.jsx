51ba95b0e23b1e557bb37ae95f8f2328123c3e74
Daniel John Baynosa <firedantsm@gmail.com>
2024-11-26 21:45:20 +0800

    Refactor Anchor and Logo components for improved theming and responsiveness; update styles for headings and anchor elements

--- src/components/Anchor.jsx
+++ src/components/Anchor.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
 
-const Anchor = (props) => {
+/**
+ * @type {(props: {
+ * 		href: String,
+ * 		target: '_blank' | '_self',
+ * 		rel: 'noopener noreferrer' | 'noreferrer',
+ * 		head: '6' | '5' | '4' | '3' | '2' | '1',
+ * 		theme: 'dark' | 'light',
+ * 		className: String,
+ * 		children: JSX.Element
+ * }) => JSX.Element}
+ */
+const Anchor = ({
+	href,
+	target,
+	rel,
+	head = '6',
+	theme,
+	className,
+	children
+}) => {
+	const anchor = (
+		<a
+			href={href}
+			target={target}
+			rel={rel}
+			className={`anchor ${className} ${theme === 'light' ? 'light' : 'dark'}`}
+		>
+			{children}
+		</a>
+	);
+
 	return (
-		<h6>
-			<a
-				href={props.href}
-				target={props.target}
-				rel={props.rel}
-				className={`anchor ${props.className}`}
-			>
-				{props.children}
-			</a>
-		</h6>
+		<>
+			{
+				head === '6' ? <h6>{anchor}</h6> :
+					head === '5' ? <h5>{anchor}</h5> :
+						head === '4' ? <h4>{anchor}</h4> :
+							head === '3' ? <h3>{anchor}</h3> :
+								head === '2' ? <h2>{anchor}</h2> :
+									head === '1' ? <h1>{anchor}</h1> :
+										<h5>{anchor}</h5>
+			}
+		</>
 	);
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ export default class Home extends React.Component {
 								this.state.mobile ? null : <Logo head='1' />
 							}
 
-							<p>Integrated task management, Pomdoro timer, and time tracking—All in one Web Application!</p>
+							<p>An all-in-one web application for <b>task management</b>, <b>time tracking</b>, and a built-in <b>Pomodoro timer</b> to supercharge your productivity.</p>
 
 							<div>
 								<Button
@@ -105,17 +105,109 @@ export default class Home extends React.Component {
 					<div>
 						<div>
 							<img src={TimeTracking} alt='Time Tracking' />
-							<p>Integrated time tracker for monitoring task duration and progress tracking.</p>
+							<p>Easily monitor task durations and track your progress with our integrated time tracker.</p>
 						</div>
 						<div>
 							<img src={RealTimeSyncronozation} alt='Real-time Synchronization' />
-							<p>Reat-time synchronization for team collaboration and project management.</p>
+							<p>Collaborate seamlessly with your team using real-time synchronization for project management.</p>
 						</div>
 						<div>
 							<img src={PomodoroTimer} alt='Pomodoro Timer' />
-							<p>Built-in Pomodoro timer to enhance focus and time management.</p>
+							<p>Boost your focus and productivity with our built-in Pomodoro timer, designed to help you manage your time effectively.</p>
 						</div>
 					</div>
+				</section>
+
+				<section className='capabilities'>
+					<article>
+						<h1>Light</h1>
+						<p>Built with a lightweight design, our web application is fast and responsive, so you can manage your tasks with ease.</p>
+						<p>Our web application is accessible from any device with an internet connection, so you can manage your tasks on the go.</p>
+
+						<div>
+							<div>
+								<img src='#/' />
+								<h3>Lighweight Design</h3>
+								<p>Our web application is designed to be lightweight and responsive, so you can manage your tasks with ease.</p>
+							</div>
+							<div>
+								<img src='#/' />
+								<h3>No Installation Required</h3>
+								<p>All you need is an internet connection to access our web application, so you can manage your tasks from anywhere.</p>
+							</div>
+						</div>
+					</article>
+					<img src='#/' />
+				</section>
+
+				<section className='capabilities'>
+					<article>
+						<h1>Fast</h1>
+						<p>Rapidly manage your tasks with our web application, designed to be fast and responsive for a seamless user experience.</p>
+						<p>Our web application is optimized for speed, so you can manage your tasks efficiently and effectively.</p>
+
+						<div>
+							<div>
+								<img src='#/' />
+								<h3>Optimized for Speed</h3>
+								<p>Our web application is designed to be fast and responsive, so you can manage your tasks efficiently.</p>
+							</div>
+							<div>
+								<img src='#/' />
+								<h3>Sleek User Interface</h3>
+								<p>Maintain focus on your tasks with our sleek user interface, designed for a seamless user experience.</p>
+							</div>
+						</div>
+					</article>
+					<img src='#/' />
+				</section>
+
+				<section className='capabilities'>
+					<article>
+						<h1>Powerful</h1>
+						<p>Take control of your tasks with our web application, designed to be powerful and feature-rich for enhanced task management.</p>
+
+						<h1>& Secure</h1>
+						<p>Designed with security in mind, so you can manage your tasks with confidence.</p>
+					</article>
+					<img src='#/' />
+				</section>
+
+				<section id='callToAction'>
+					<h3>Are you ready?</h3>
+					<p>Sign up now and start managing your tasks more efficiently.</p>
+					<Button
+						label='Get Started'
+						href='/signUp'
+						type='CallToAction'
+						head='6'
+						theme='light'
+					/>
+
+					<svg viewBox='0 0 1440 854'>
+						<path d='M1436.04 854H-0.875604C-0.875604 765.5 -0.875702 224 -0.999505 131.265C197.162 169.746 392.739 168.245 627 100.802C1044.5 -19.3962 1416 -5.50003 1440.5 11C1465 27.5 1436.04 854 1436.04 854Z' fill='var(--color-secondary)' />
+					</svg>
+				</section>
+
+				<section id='footer'>
+					<div>
+						<span>© 2024<br />All rights reserved.</span>
+
+						<Logo
+							head='6'
+							theme='light'
+						/>
+
+						<div>
+							<Anchor theme='light' href='/' className='active'>Home</Anchor>
+							<Anchor theme='light' href='/about'>About</Anchor>
+							<Anchor theme='light' href='/contact'>Contact</Anchor>
+						</div>
+					</div>
+
+					<svg viewBox='0 0 1440 354'>
+						<path d='M925.643 77C1324.92 12.212 1403.1 31.7986 1440 0V354H-1V141.949C332.005 210.771 526.366 141.788 925.643 77Z' fill='var(--color-primary)' />
+					</svg>
 				</section>
 			</>
 		);
