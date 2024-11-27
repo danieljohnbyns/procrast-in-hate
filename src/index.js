de77a50775d776f1b4633317d85c29125a2debab
Daniel John Baynosa <firedantsm@gmail.com>
2024-11-27 14:53:28 +0800

    Add react-router-dom for routing; implement basic routing with Home and SignUp pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+	BrowserRouter,
+	HashRouter,
+	Routes,
+	Route,
+} from 'react-router-dom';
 
 import './fonts/fontFace.css';
 import './styles/index.css';
 
 import Home from './pages/Home';
+import SignUp from './pages/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
-		<Home />
-	</React.StrictMode>
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<Home />} />
+
+			<Route path='/signup' element={<SignUp />} />
+		</Routes>
+	</BrowserRouter>
 );
