a50eaa7bfbb26f9cfabeab386b0098136c2d4279
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-05-30 22:53:52 +0800

    Fix sign-in redirect to dashboard

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -110,7 +110,7 @@ export default class SignIn extends React.Component {
 										type: 'SIGN_IN'
 									});
 
-									// window.location.href = '/dashboard/';
+									window.location.href = '/dashboard/';
 								}}
 							/>
 							<p><a href='/forgotPassword'>Forgot your password?</a></p>
