beead04a51f2d9705565eeb3a6012d0128afc0a4
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-05-30 22:00:26 +0800

    Fix backend url

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,7 +1,7 @@
 
 const globals = {
-	API_URL: 'http://54.255.205.110:5050',
-	WEBSOCKET_URL: 'wss://54.255.205.110:5050',
+	API_URL: 'http://18.141.58.57:5050',
+	WEBSOCKET_URL: 'wss://18.141.58.57:5050',
 	socket: null
 };
 
