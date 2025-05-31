145e63a4c5df987d62cd3d15003b13ee86525f7e
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-05-31 12:45:51 +0800

    Update API and WebSocket URLs to use secure connections

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,7 +1,7 @@
 
 const globals = {
-	API_URL: 'http://18.140.164.168:5050',
-	WEBSOCKET_URL: 'ws://18.140.164.168:5050',
+	API_URL: 'https://18.140.164.168/api/',
+	WEBSOCKET_URL: 'wss://18.140.164.168/api/',
 	socket: null
 };
 
