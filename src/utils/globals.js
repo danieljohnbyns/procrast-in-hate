17ddd92f996404b5c2cfae8fce37d53024160c0d
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-05-31 12:39:17 +0800

    Revert API and WebSocket URLs to non-secure connections

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,7 +1,7 @@
 
 const globals = {
-	API_URL: 'https://18.140.164.168',
-	WEBSOCKET_URL: 'wss://18.140.164.168',
+	API_URL: 'http://18.140.164.168:5050',
+	WEBSOCKET_URL: 'ws://18.140.164.168:5050',
 	socket: null
 };
 
